eac08aee6f7399b8c6773cd63d690cd475624e8f
pablocharbonnier <134537190+pablocharbonnier@users.noreply.github.com>
2023-08-01 10:59:19 -0300

    cambios_entrega-dos

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
-let resultado = 0
-let saludo = "muchas gracias por elgirnos"
-let ticket = "detalle de su pedido: \n"
-
-let rta = ""
 
 
-do{
-    
-    let articulo = prompt("ingrese un articulo")
-    let precio = Number(prompt("ingrese el precio del articulo")) 
+const simulador = {
+  resultado: 0,
+  saludo: "¡Muchas gracias por elegirnos!",
+  ticket: "Detalle de su pedido:\n",
 
-    resultado = resultado + precio
-    ticket = ticket +"\n" +articulo+"\t\t$"+ precio
+  productosTecnologicos: [
+    { nombre: "case", precio: 500 },
+    { nombre: "cargador", precio: 1100 },
+    { nombre: "teclado inalambrico", precio: 1700 },
+    { nombre: "mouse inalambrico", precio: 600 },
+    { nombre: "soporte para celular", precio: 1200 },
+    { nombre: "auriculares", precio: 2200 },
+  ],
 
-    rta = prompt("¿desea continuar? , para terminar con su pedido ingrese 'salir'" )
+  iniciarSimulador: function (calcularTotal) {
+    let rta = "";
+    do {
+      let productoElegido = prompt("Ingrese el número del producto que desea:\n" +
+        this.mostrarProductos());
+      let precio = this.productosTecnologicos[productoElegido - 1].precio;
+      let articulo = this.productosTecnologicos[productoElegido - 1].nombre;
+      this.resultado += precio;
+      this.ticket += "\n" + articulo + "=========$" + precio;
+      rta = prompt("¿Desea continuar? Para terminar con su pedido, ingrese 'salir'");
+    } while (rta !== "salir");
 
+    let total = calcularTotal(this.resultado);
+    alert(this.ticket +
+      "\nTotal: $==========" + this.resultado +
+      "\n\nTotal con descuento: $==========" + total);
+  },
+  mostrarProductos: function () {
+    let listaProductos = "";
+    this.productosTecnologicos.forEach((producto, index) => {
+      listaProductos += `${index + 1}. ${producto.nombre} ($${producto.precio})\n`;
+    });
+    return listaProductos;
+  },
 
-}while(rta != "salir")
-
-alert(ticket + "\n\ntotal $"+resultado)
-
-if(resultado >= 2000){
-    //descuento del 10%
-    resultadoConDescuento = resultado * 0.9
-
-    alert(ticket
-        +"\ntotal: $"+ resultado
-        +"\n\n\ntotal con descuento:"+ resultadoConDescuento)
-}else{
-    alert(ticket
-        +"\n\ntotal: $"+ resultado)
-}
-    
-
-function saludar(){
-    let saludo = "muchas gracias por elegirnos"
-    console.log(saludo)
+  saludar: function () {
+    alert(this.saludo);
   }
-saludar(alert(saludo))
+};
 
+const calcularTotalConDescuento = (total) => {
+  if (total >= 2000) {
+    return total * 0.9;
+  } else {
+    return total;
+  }
+};
+
+simulador.saludar();
+simulador.iniciarSimulador(calcularTotalConDescuento);
